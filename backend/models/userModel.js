ccf79b720a7c2ada1e4e00f0d0c09a32fa2eab6f
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-11-29 17:03:47 +0200

    finish signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const becrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   userName: {
@@ -25,12 +26,49 @@ const userSchema = new mongoose.Schema({
   emailAddress: { type: String, required: true, lowercase: true, unique: true },
   role: {
     type: String,
-    enum: ['Manager', 'Fan', 'Site Administrator'],
+    enum: ['manager', 'fan', 'admin'],
     required: true
   },
   isPending: { type: Boolean, default: true, required: true },
   createdIn: { type: Date, default: Date.now, required: true }
 });
+
+//signup
+userSchema.statics.signup = async function(
+  userName,
+  password,
+  firstName,
+  lastName,
+  birthDate,
+  gender,
+  city,
+  address,
+  emailAddress,
+  role
+) {
+  const exists = await this.findOne({ userName });
+
+  if (exists) {
+    throw Error('this username already exists!');
+  }
+  const saltRounds = 10;
+  const salt = await becrypt.genSalt(saltRounds);
+  const hash = await becrypt.hash(password.toString(), salt);
+  const user = await this.create({
+    userName,
+    password: hash,
+    firstName,
+    lastName,
+    birthDate,
+    gender,
+    city,
+    address,
+    emailAddress,
+    role
+  });
+
+  return user;
+};
 
 const userModel = mongoose.model('User', userSchema);
 
