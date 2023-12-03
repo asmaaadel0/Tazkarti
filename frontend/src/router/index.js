77f902f79a9191c346d38733086605981b780b67
Asmaa Adel <adelasmaa333@gmail.com>
2023-12-04 01:55:36 +0200

    add users and requests views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import LoginView from "../views/auth/LoginView.vue";
 import SignupView from "../views/auth/SignupView.vue";
 import MatchesView from "../views/home/MatchesView.vue";
 import EditProfileView from "../views/user/EditProfileView.vue";
+import UsersView from "../views/admin/UsersView.vue";
+import RequestsView from "../views/admin/RequestsView.vue";
 
 import NotFound from "../views/NotFound.vue";
 import InternalServerError from "../views/InternalServerError.vue";
@@ -28,6 +30,16 @@ const routes = [
     path: "/edit-profile",
     name: "edit-profile",
     component: EditProfileView,
+  },
+  {
+    path: "/users",
+    name: "users",
+    component: UsersView,
+  },
+  {
+    path: "/requests",
+    name: "requests",
+    component: RequestsView,
   },
   {
     path: "/enternal-server-error",
---
+++ frontend/src/views/admin/RequestsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row">
+    <div class="child1">test</div>
+    <div class="child2">test</div>
+  </div>
+</template>
+<script>
+export default {
+  beforeMount() {
+    if (localStorage.getItem("role") != "admin") {
+      this.$router.push("/");
+    }
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  height: auto;
+  min-height: 100vh;
+}
+.child1 {
+  flex-basis: 50%;
+  width: 50%;
+  text-align: center;
+  background-color: var(--color-primary);
+}
+.child2 {
+  flex-basis: 50%;
+  width: 50%;
+  text-align: center;
+  margin-top: 5rem;
+}
+
+@media (max-width: 500px) {
+  .row {
+    flex-direction: column;
+  }
+}
+</style>
